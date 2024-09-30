a5d6d2c9590e793d0d8dbda22f2dadd81950020e
임동훈 <snowcrab382@naver.com>
2024-09-30 14:23:29 +0900

    Title: 외주 수익 최대화하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240930/외주 수익 최대화하기/max-of-outsourcing-profit.py
+++ 240930/외주 수익 최대화하기/max-of-outsourcing-profit.py
@@ -7,5 +7,5 @@ dp = [0] * (n + 1)
 dp[0] = 0
 for i, work in enumerate(works):
     t, p = work[0], work[1]
-    dp[i + t] = max(dp[i + t], dp[i] + p)
+    dp[i + t] = max(dp[i + t], max(dp[:i + 1]) + p)
 print(max(dp))
