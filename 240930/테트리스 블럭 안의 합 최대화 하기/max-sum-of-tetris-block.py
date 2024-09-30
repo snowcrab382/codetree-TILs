333583ecd06de7ed0d2a9c41ec51153d1d326b5e
임동훈 <snowcrab382@naver.com>
2024-09-30 11:46:43 +0900

    Title: 테트리스 블럭 안의 합 최대화 하기, Time: 143ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240930/테트리스 블럭 안의 합 최대화 하기/max-sum-of-tetris-block.py
+++ 240930/테트리스 블럭 안의 합 최대화 하기/max-sum-of-tetris-block.py
@@ -4,10 +4,9 @@ graph = [list(map(int, input().split())) for _ in range(n)]
 
 # 4 X 4 범위 내에서 최대값 구하기
 def check():
-    global result
     # 길이가 4인 직선 체크 
     for i in range(4):
-        result = max(result, sum(reduced[i]))
+        answer.append(sum(reduced[i]))
 
     comb = [(0,1), (0,2), (0,3), (0,5), (1,2), (2,3), (2,5), (3,4), (3,5), (4,5)]
     # 2 X 3 도형 중 2개를 뺀 것들 중 최대 체크(2,5번 사용 케이스는 제외)
@@ -20,7 +19,7 @@ def check():
             tmp_blocks_sum = 100000000
             for x, y in comb:
                 tmp_blocks_sum = min(tmp_blocks_sum, blocks[x] + blocks[y])
-            result = max(result, sum(blocks) - tmp_blocks_sum)
+            answer.append(sum(blocks) - tmp_blocks_sum)
 
 # 2차원 배열 90도 회전
 def rotate(graph):
@@ -40,9 +39,9 @@ for i in range(n - 3):
         if cnt > MAX:
             reduced = tmp
 
-result = 0
+answer = []
 check()
 reduced = rotate(reduced)
 check()    
 
-print(result)
+print(max(answer))
