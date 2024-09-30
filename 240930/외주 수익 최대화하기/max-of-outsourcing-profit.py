84bef27b4c6c22127a1aa9fc597a0be00356d24a
임동훈 <snowcrab382@naver.com>
2024-09-30 14:15:50 +0900

    Title: 외주 수익 최대화하기, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240930/외주 수익 최대화하기/max-of-outsourcing-profit.py
@@ -0,0 +1,11 @@
+# input
+n = int(input())
+works = [list(map(int,input().split())) for i in range(n)]
+
+# solution
+dp = [0] * (n + 1)
+dp[0] = 0
+for i, work in enumerate(works):
+    t, p = work[0], work[1]
+    dp[i + t] = max(dp[i + t], dp[i] + p)
+print(max(dp))
