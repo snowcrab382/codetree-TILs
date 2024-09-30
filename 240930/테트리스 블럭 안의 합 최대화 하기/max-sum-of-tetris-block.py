9957757289bb4fa3f4dd7774dbf3b1ebc4379ef4
임동훈 <snowcrab382@naver.com>
2024-09-30 12:09:05 +0900

    Title: 테트리스 블럭 안의 합 최대화 하기, Time: 256ms, Memory: 26MB, Status: Passed - CodeTree

--- 240930/테트리스 블럭 안의 합 최대화 하기/max-sum-of-tetris-block.py
+++ 240930/테트리스 블럭 안의 합 최대화 하기/max-sum-of-tetris-block.py
@@ -1,49 +1,28 @@
-#input
-n, m = map(int, input().split())
-graph = [list(map(int, input().split())) for _ in range(n)]
+n, m = map(int,input().split())
+graph = [list(map(int,input().split())) for _ in range(n)]
 
-# 4 X 4 범위 내에서 최대값 구하기
-def check():
-    global result
-    # 길이가 4인 직선 체크 
-    for i in range(4):
-        result = max(result,sum(reduced[i]))
+blocks = [[(0,1),(0,2),(0,3)],[(1,0),(2,0),(3,0)],		#연속 일자 모형
+		[(0,1),(1,0),(1,1)],							#정사각형
+        [(1,0),(1,1),(2,0)],[(1,0),(2,0),(1,-1)],[(0,1),(0,2),(-1,1)],[(0,1),(0,2),(1,1)],	#가운데 툭튀 형
+        [(1,0),(1,1),(2,1)],[(1,0),(0,1),(-1,1)],[(0,1),(-1,1),(-1,2)],[(0,1),(1,1),(1,2)],	#계단형
+        [(1,0),(2,0),(2,1)],[(0,1),(-1,1),(-2,1)],[(0,1),(1,0),(2,0)],[(0,1),(1,1),(2,1)],	#ㄱ,ㄴ 형
+        [(0,1),(0,2),(-1,2)],[(1,0),(1,1),(1,2)],[(0,1),(0,2),(1,2)],[(0,1),(0,2),(1,0)]]
 
-    comb = [(0,1), (0,2), (0,3), (0,5), (1,2), (2,3), (2,5), (3,4), (3,5), (4,5)]
-    # 2 X 3 도형 중 2개를 뺀 것들 중 최대 체크(2,5번 사용 케이스는 제외)
-    for i in range(3):
-        for j in range(2):
-            blocks = []
-            for a in range(i, i + 2):
-                for b in range(j, j + 3):
-                    blocks.append(reduced[a][b])
-            tmp_blocks_sum = 100000000
-            for x, y in comb:
-                tmp_blocks_sum = min(tmp_blocks_sum, blocks[x] + blocks[y])
-            result = max(result, sum(blocks) - tmp_blocks_sum)
 
-# 2차원 배열 90도 회전
-def rotate(graph):
-    return list(map(list, zip(*graph[::-1])))
-
-# 4 X 4로 탐색범위 좁히기
-reduced = [[0] * 4 for _ in range(4)]
-MAX = 0
-for i in range(n - 3):
-    for j in range(m - 3):
-        tmp = [[0] * 4 for _ in range(4)]
-        cnt = 0
-        for x in range(4):
-            for y in range(4):
-                tmp[x][y] = graph[i + x][j + y]
-                cnt += tmp[x][y]
-        if cnt > MAX:
-            MAX = cnt
-            reduced = tmp
-
-result = 0
-check()
-reduced = rotate(reduced)
-check()    
-
-print(result)
+answer = 0
+for i in range(n):
+    for j in range(m):
+        for block in blocks:
+            tmp = graph[i][j]
+            flag = True
+            for dx, dy in block:
+                nx = i + dx
+                ny = j + dy
+                if 0 <= nx < n and 0 <= ny < m:
+                    tmp += graph[nx][ny]
+                else:
+                    flag = False
+                    break
+            if flag:
+                answer = max(answer, tmp)
+print(answer)
